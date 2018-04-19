4a9ede6439b8d7018a8019b337c9ff5593570e62
Michael Figueroa <mfigueroa14@cnm.edu>
2018-04-18 23:29:45 -0600

    pwp goals

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # michaels-pwp
+This project will help me define the direction of my Personal website.
---
+++ public_html/documentation/milestone-1.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+	<meta charset="UTF-8">
+	<title>Michael's PWP</title>
+	<body>
+		<h1>Michael's PWP</h1>
+		<h2>Purpose</h2>
+		<p>I would like this PWP help me get a career in I.T. by showing employers my educational and professional background.</p>
+		<h2>Audience</h2>
+		<p>Any one can visit my site but, I want the main audience to be future potential employers and professional peers.
+			The site will be designed to show anyone who visits it, my educational and professional background, As well as
+			a list of my interests and a short description about me and who I am</p>
+		<h2>Goal</h2>
+		<p>The goal that i want for this site is to show future potential employers my skill set(s) as well as it be a site
+			where anyone can visit and not only see my professional skill set(s) but as well as my personal goals and interests.
+		Overall I want this to be a site about me where people who visit can learn about what kind of person I am and where
+		employers can look at my professional history and potentially hire me.</p>
+	</body>
+</html>
