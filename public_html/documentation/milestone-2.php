987e77e5677595b2d0814640e270dabdc8a2c088
Michael Figueroa <mfigueroa14@cnm.edu>
2018-05-02 22:07:35 -0600

    pwp milestone 2 content stratigy

--- public_html/documentation/milestone-2.php
+++ public_html/documentation/milestone-2.php
@@ -4,6 +4,13 @@
 	<title>Michael's PWP</title>
 	<body>
 		<h1>Wireframe</h1>
+		<p>I want this site to be a site where employers can look at my professional skills as well as find out what kind
+			of person I am and what my personality is like. When they get to the homepage of the site it will show an
+			"About Me" page where it will have information on just that. It will have a description on what kind of person
+			I am along with my interests and personality. At the top of the page it will have little tabs where they can
+			navigate the rest of the site and choose to se my Portfolio, Resume, and my Contact information. The same goes
+			for the mobile version of the site the only major difference being the tabs will com up from a little side bar
+			when a "Hamburger" button is tapped.</p>
 		<img src="/images/wireframe.jpeg">
 	</body>
 </html>
