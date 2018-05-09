8d61c6475ac7c6cb0b56da31eb469d7836293ae4
Michael Figueroa <mfigueroa14@cnm.edu>
2018-05-09 02:09:35 -0600

    pwp milestone 2 content Strategy update

--- public_html/documentation/milestone-2.php
+++ public_html/documentation/milestone-2.php
@@ -3,14 +3,20 @@
 	<meta charset="UTF-8">
 	<title>Michael's PWP</title>
 	<body>
-		<h1>Wireframe</h1>
+		<h1>Wireframe & Content Strategy</h1>
 		<p>I want this site to be a site where employers can look at my professional skills as well as find out what kind
-			of person I am and what my personality is like. When they get to the homepage of the site it will show an
-			"About Me" page where it will have information on just that. It will have a description on what kind of person
-			I am along with my interests and personality. At the top of the page it will have little tabs where they can
-			navigate the rest of the site and choose to se my Portfolio, Resume, and my Contact information. The same goes
-			for the mobile version of the site the only major difference being the tabs will com up from a little side bar
-			when a "Hamburger" button is tapped.</p>
+			of person I am and what my personality is like. It will have a description on what kind of person
+			I am along with my interests and personality.</p>
+		<p>When they get to the top of the site it will show an "About Me" section where it will have information on just
+			that. At the top of the page where the header is, it will have little tabs where they can
+			navigate the rest of the site and choose to see my Portfolio, Resume, and my Contact information.</p>
+			<p>As you scroll down the screen the second section is going to have my Portfolio with information on my
+			Quantitative & Qualitative skills & accomplishments. Has click-able links</p>
+		<p>The third section will have a copy of my Resume so Employers can see my professional accomplishments and passed
+			experience as well as my professional skills.Also has click-able links</p>
+		<p>The fourth and last section will be a section where employers can contact me. There will be two boxes where
+			they can put their email address and send me email messages. There will also be social media icons that when clicked on will
+			send them to my profile that corresponds to said social media outlets.</p>
 		<img src="../../images/wireframe.png">
 	</body>
 </html>
