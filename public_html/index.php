fae2b538cecf23fe4fe03c273716763a04670a52
Michael Figueroa <mfigueroa14@cnm.edu>
2018-05-20 23:30:28 -0600

    index.php added and redume image added

---
+++ public_html/index.php
@@ -0,0 +1,114 @@
+<!DOCTYPE>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<title>Michael's Website</title>
+	</head>
+	<body>
+		<header>Michael Figueroa</header>
+		<h1>About Me</h1>
+		<p>My name is Michael Figueroa and i am a low level computer programmer (so far). I enjoy working with technology,
+			playing video games, and listening to music in my spare time.</p>
+		<h1>Portfolio</h1>
+		<p>
+		<p>Career buddy: Jully Hawkins</p>
+		<h2>Links:</h2>
+		<ul>
+			<li><a href="mailto:spider811games@gmail.com">Email</a></li>
+			<li><a href="https://github.com/Mike1f">GitHub</a></li>
+			<li><a href="https://www.linkedin.com/in/michaelfigueroa811">LinkedIn</a></li>
+			<li><a href="https://twitter.com/spider811games">Twitter</a></li>
+			<li><a href="https://docs.google.com/document/d/1i26k884RdvxWO0GBUSqU7maovqeftOJIJ6BefOKR8Q8/edit?usp=sharing">My Resume</a></li>
+		</ul>
+		<h2>Quantitative Accomplishments</h2>
+		<ol>
+			<li>I worked on a website for The Department of Workforce Solutions called
+				<a href="https://www.dws.state.nm.us/whyiwork/">Why I Work</a>
+				when I was an Intern there. I worked on this site without having any coding experience so I self taught
+				myself how to edit the code. I also </li>
+			<li>When I was in middle school I was in a robotics club where I built a robot that i had programed to bring
+				ping pongs from point A to point B and back. My robot was even on the news and i was also interviewed about
+				it for a news story.</li>
+			<li>I entered a 10 week coding boot-camp where I learned how to code websites.</li>
+		</ol>
+		<h2>Qualitative Features</h2>
+		<ul>
+			<li>I'm a good listener.</li>
+			<li>I'm organized</li>
+			<li>I'm punctual</li>
+		</ul>
+		<h2>Golden Circle</h2>
+
+		<ul>
+			<li>What: Get a career within I.T. </li>
+			<li>How: Get an education withing the I.T. field.</li>
+			<li>Why: I enjoy working with computers and technology and I can help other people with technical problems.</li>
+		</ul>
+
+		<h2>Elevator Pitch</h2>
+
+		<p>Hello, my name is Michael Figueroa. I'm going to get a job in I.T. because I love technology and I want to help
+			people with their technical problems. I'm taking a coding boot camp and I'm planning on taking more classes
+			within the I.T. field to achieve this career choice.</p>
+
+		<h2>Weekly Evaluation</h2>
+
+		<ul>Week 0 of the boot camp was really good. We reviewed creating and deploying repositories as well as learning
+			how to create Personas and Use/Case Interaction Flows. We gor into groups when learning these subjects so not
+			only did I learn a lot, I also got to talk with and interact with my classmates which was a great experience.
+			Looking forward to next week.</ul>
+
+		<ul>Week 1 of the boot camp went great. We got to learn about Object Oriented PHP. wel learned about classes,
+			getter and setter methods, traits, and .json files. As for the Pro Dev side of things we got ton  do an activity
+			where we picked two random pictures that had to represent where we were in the present and where we wanted to
+			be 3 years in the future. It was a really fun activity because not only did I get more of an idea on what I
+			wanted in my future, I also got to see what my peers were at now and where they wanted for their future, so it
+			was really interesting to see that.</ul>
+
+		<ul>Week 2 of the Boot camp went pretty good. I am getting a little stress from the amount of work but I'm managing
+			with the help of my teachers and peers. this week what we did wqas finish up our Data Design project as well as
+			start a new assignment with CSS where we had to edit contents of a simple html page to look more professional.
+			We also got into our capstone groups where we are going to work on the website that we have to present at graduation
+			which is really cool and im excited about.</ul>
+		<ul>
+			<li>I'm a good listener.</li>
+			<li>I'm organized</li>
+			<li>I'm punctual</li>
+		</ul>
+		<h2>Golden Circle</h2>
+
+		<ul>
+			<li>What: Get a career within I.T. </li>
+			<li>How: Get an education withing the I.T. field.</li>
+			<li>Why: I enjoy working with computers and technology and I can help other people with technical problems.</li>
+		</ul>
+
+		<h2>Elevator Pitch</h2>
+
+		<p>Hello, my name is Michael Figueroa. I'm going to get a job in I.T. because I love technology and I want to help
+			people with their technical problems. I'm taking a coding boot camp and I'm planning on taking more classes
+			within the I.T. field to achieve this career choice.</p>
+
+		<h2>Weekly Evaluation</h2>
+
+		<ul>Week 0 of the boot camp was really good. We reviewed creating and deploying repositories as well as learning
+			how to create Personas and Use/Case Interaction Flows. We gor into groups when learning these subjects so not
+			only did I learn a lot, I also got to talk with and interact with my classmates which was a great experience.
+			Looking forward to next week.</ul>
+
+		<ul>Week 1 of the boot camp went great. We got to learn about Object Oriented PHP. wel learned about classes,
+			getter and setter methods, traits, and .json files. As for the Pro Dev side of things we got ton  do an activity
+			where we picked two random pictures that had to represent where we were in the present and where we wanted to
+			be 3 years in the future. It was a really fun activity because not only did I get more of an idea on what I
+			wanted in my future, I also got to see what my peers were at now and where they wanted for their future, so it
+			was really interesting to see that.</ul>
+
+		<ul>Week 2 of the Boot camp went pretty good. I am getting a little stress from the amount of work but I'm managing
+			with the help of my teachers and peers. this week what we did wqas finish up our Data Design project as well as
+			start a new assignment with CSS where we had to edit contents of a simple html page to look more professional.
+			We also got into our capstone groups where we are going to work on the website that we have to present at graduation
+			which is really cool and im excited about.</ul>
+		<h1>Resume</h1>
+		<img src="../../images/Resumeimg.png">
+	</body>
+</html>
